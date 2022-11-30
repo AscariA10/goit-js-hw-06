a212eda52489979d1d912af2183a616c797ffc28
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2022-11-30 13:20:14 +0200

    all tasks complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const inputRef = document.querySelector('input');
+const gallery = document.querySelector('#controls');
+const galleryPlace = document.querySelector('#boxes');
+const createButtonRef = document.querySelector('[data-create]');
+const destroyButtonRef = document.querySelector('[data-destroy]');
+
+const quantityArray = [];
+
+createButtonRef.addEventListener('click', creationOfCollection);
+destroyButtonRef.addEventListener('click', () => {
+   galleryPlace.innerHTML = '';
+   inputRef.value = '';
+   quantityArray.splice(0,quantityArray.length);
+   console.log(quantityArray)
+});
+
+function creationOfCollection() {
+   const elementsQuantity = inputRef.value;
+
+   for (let i = 0; i < elementsQuantity; i += 1) {
+      let galleryIcon = document.createElement('div');
+      galleryIcon.style.width = `${30 + i * 10}px`;
+      galleryIcon.style.height = `${30 + i * 10}px`;
+      galleryIcon.style.backgroundColor = getRandomHexColor();
+      quantityArray.push(galleryIcon);
+      
+   }
+   console.log(quantityArray)
+   galleryPlace.append(...quantityArray);
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
